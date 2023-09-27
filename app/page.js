e9b90c0095a01e38fb8a816681820ec9c7a9eeeb
Ghullam Mustafa <gmustafawaince180@gmail.com>
2023-09-28 00:16:00 +0500

    complete

--- app/page.js
+++ app/page.js
@@ -1,9 +1,26 @@
-import Image from 'next/image'
+const fetchPost = async ()=>{
+  const res = await fetch("http://localhost:3000/api/post",{cache:"no-cache"})
+  const response = await res.json()
+  return response
+}
 
-export default function Home() {
+export default async function Home() {
+  const post = await fetchPost()
   return (
     <>
-    <h1>jjj</h1>
+    <h1 className=" text-center">posts</h1>
+    {
+      post.map((posts)=>{
+        return (
+        <>
+        <h1>{posts.title}</h1>
+        <p>{posts.descrition}</p>
+        {/* <p>{posts.title}</p> */}
+        <hr />
+        </>)
+      })
+    }
+
     </>
   )
 }
